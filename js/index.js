05e14fe690907bbdb81d533573aa3d0b1ac0a66f
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2025-02-21 22:31:18 +0100

    .

--- js/index.js
+++ js/index.js
@@ -1,22 +1,64 @@
+const genderCheckboxes = document.querySelectorAll(".gender-checkbox");
+const ageText = document.querySelector(".age-text");
+const finalTextArea = document.getElementById("result-text");
+const notNormalHome = document.querySelectorAll(".other-forms-of-family");
+const homeDiv = document.querySelector(".family-input-div");
+let pronoun = "";
+let age = "";
+let textArea = [];
+let ka;
 //DOMContentLoaded - ruchamia się, gdy cały HTML został załadowany i przetworzony przez przeglądarkę
 document.addEventListener("DOMContentLoaded", function () {
-  const checkboxes = document.querySelectorAll(".gender-checkbox");
-  let pronoun = "";
-
-  checkboxes.forEach((checkbox) => {
+  genderCheckboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", function () {
       if (this.checked) {
         if (this.value === "Kobieta") {
           pronoun = "ona";
+          ka = "ka";
         } else if (this.value === "Mężczyzna") {
           pronoun = "on";
         }
 
-        checkboxes.forEach((cb) => {
+        genderCheckboxes.forEach((cb) => {
           if (cb !== this) cb.checked = false;
         });
 
         console.log("Wybrany zaimek:", pronoun);
+      }
+    });
+  });
+  ageText.addEventListener("change", () => {
+    if (ageText.value) {
+      textArea.push({ wiek: `Pacjent${ka} lat ${ageText.value}` });
+      console.log(textArea);
+      if (finalTextArea) {
+        finalTextArea.value = textArea
+          .map((patient) => `${patient.wiek}`)
+          .join("\n");
+      } else {
+        console.error("Nie znaleziono textarea!");
+      }
+    }
+  });
+  notNormalHome.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+      let homeInput;
+
+      if (checkbox.checked) {
+        // Sprawdzamy, czy już mamy input w divie, aby uniknąć duplikatów
+        if (!homeDiv.querySelector("input")) {
+          homeInput = document.createElement("input"); // Tworzymy nowy input
+          homeInput.type = "text";
+          homeInput.placeholder = "podaj więcej informacji";
+          homeInput.id = "homeInput";
+          homeDiv.appendChild(homeInput); // Dodajemy input do div
+        }
+      } else {
+        // Usuwamy input, jeśli checkbox jest odznaczony
+        homeInput = homeDiv.querySelector("input");
+        if (homeInput) {
+          homeDiv.removeChild(homeInput); // Usuwamy input z div
+        }
       }
     });
   });
